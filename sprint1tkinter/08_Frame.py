de0e82b0c25d81fa27cd33e330ca209bb3bcf1b8
Gabriela Vázquez <gvazquezi24@fpcoruna.afundacion.org>
2025-10-27 17:31:30 +0100

    Corrección en Ejercicio 8

--- sprint1tkinter/08_Frame.py
+++ sprint1tkinter/08_Frame.py
@@ -11,9 +11,10 @@ Puedes organizar los widgets con grid() dentro de cada Frame."""
 import tkinter as tk
 
 def mostrar():
-    mensaje.config(text=f"{entry1.get()}")
+    etiqueta2.config(text=f"Has escrito: {entry1.get()}")
 def borrar():
-    mensaje.config(text=f"")
+    entry1.delete(0, tk.END)
+    etiqueta2.config(text=f"Has escrito:")
 
 root = tk.Tk()
 root.title("08_Frames_Gabriela.A.V.I")
@@ -27,17 +28,15 @@ frame2 = tk.Frame(root, relief="sunken", borderwidth=1)
 frame2.pack(padx=10, pady=10)
 
 #Crear widgets dentro de Frame1
-etiqueta1 = tk.Label(frame1, text="Primera etiqueta")
+etiqueta1 = tk.Label(frame1, text="Introduce texto")
 etiqueta1.grid(row=0, column=0, padx=5, pady=5)
-etiqueta2 = tk.Label(frame1, text="Segunda etiqueta")
-etiqueta2.grid(row=1, column=0, padx=5, pady=5)
+etiqueta2 = tk.Label(frame1, text="Has escrito:")
+etiqueta2.grid(row=2, column=0, padx=5, pady=5)
 
 entry1 = tk.Entry(frame1)
-entry1.grid(row=2, column=0, padx=5, pady=5)
+entry1.grid(row=1, column=0, padx=5, pady=5)
 
 #Crear widgets dentro de Frame2
-mensaje = tk.Label(frame2, text="")
-mensaje.grid(row=0, column=0, columnspan=2, pady=10)
 
 boton1 = tk.Button(frame2, text="Mostrar", command=mostrar)
 boton1.grid(row=1, column=0, padx=10, pady=10)
