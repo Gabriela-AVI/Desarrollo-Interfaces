0728c402528b4ab7c66fb39df5bf5cf2a000f2ee
Gabriela Vázquez <gvazquezi24@fpcoruna.afundacion.org>
2025-11-06 13:20:49 +0100

    Ejercicio 2 Layout

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,14 +1,31 @@
 package com.example.mycatalog;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CatalogActivity extends AppCompatActivity {
 
+    private Button NavegarDetalle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        // Botón de xml
+        NavegarDetalle = findViewById(R.id.NavegarDetalle);
+
+        // Click para abrir DetailActivity
+        NavegarDetalle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
