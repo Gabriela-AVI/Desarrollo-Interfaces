95fb55d377c0c865400f631d63b92342ecbc06f0
Gabriela Vázquez <gvazquezi24@fpcoruna.afundacion.org>
2025-11-10 17:46:20 +0100

    Ejercicio 5 Layout

--- sprint3android/app/build.gradle.kts
+++ sprint3android/app/build.gradle.kts
@@ -29,14 +29,22 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
+    implementation("androidx.navigation:navigation-fragment:2.8.0")
+    implementation("androidx.navigation:navigation-ui:2.8.0")
+    implementation("com.google.android.material:material:1.12.0")
 
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
