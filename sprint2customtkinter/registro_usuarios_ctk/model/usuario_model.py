fff76c75c9d63189110118b7e2b4ca4692c692ff
Gabriela Vázquez <gvazquezi24@fpcoruna.afundacion.org>
2025-11-16 20:44:22 +0100

    Fase 3: CSV cargar

--- sprint2customtkinter/registro_usuarios_ctk/model/usuario_model.py
+++ sprint2customtkinter/registro_usuarios_ctk/model/usuario_model.py
@@ -42,7 +42,44 @@ class GestorUsuarios:
 
         except Exception as e:
             print("Error al guardar CSV:", e)
-            return False  #
+            return False  #fallo
 
     def cargar_csv(self, ruta: str = "usuarios.csv"):
-        ...
+        # Cargar usuarios desde CSV
+        try:
+            with open(ruta, "r", encoding="utf-8", newline="") as f:
+                reader = csv.reader(f)
+
+                next(reader, None) # Leer cabecera y descartarla
+
+                self._usuarios.clear()  # Vaciar lista actual
+
+                for fila in reader:
+                    # Fila válida --> EXACTAMENTE 4 columnas
+                    if len(fila) != 4:
+                        print("Fila inválida saltada:", fila)
+                        continue
+
+                    nombre, edad, genero, avatar = fila
+
+                    # Edad debe ser número
+                    try:
+                        edad = int(edad)
+                    except ValueError:
+                        print("Edad inválida en fila:", fila)
+                        continue
+
+                    # Crear usuario y añadirlo
+                    user = Usuario(nombre, edad, genero, avatar)
+                    self._usuarios.append(user)
+
+            return True  # éxito
+
+        except FileNotFoundError:
+            print("Archivo no encontrado")
+            return False
+
+        except Exception as e:
+            print("Error al cargar CSV:", e)
+            return False
+
